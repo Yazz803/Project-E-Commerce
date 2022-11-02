7ff31530e44149e21f5603cf9561caee2ccd68f6
Yazz803 <yazidakbar08@gmail.com>
2022-11-02 23:08:36 +0700

    add model Order, OrderController, and fix UI shopping cart

---
+++ app/Http/Controllers/User/OrderController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Models\Order;
+use Illuminate\Http\Request;
+use RealRashid\SweetAlert\Facades\Alert;
+
+class OrderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'product_id' => 'required',
+            'quantity' => 'required',
+        ]);
+
+        $validatedData['user_id'] = auth()->user()->id;
+        // jika product id nya sama dengan product id yang ada di database, maka jalankan code untuk menambah product
+        if (Order::where('product_id', $validatedData['product_id'])->exists()) {
+            $order = Order::where('product_id', $validatedData['product_id'])->first();
+            $order->quantity += $validatedData['quantity'];
+            $order->save();
+        } else {
+            Order::create($validatedData);
+        }
+
+        Alert::success('Berhasil Memesan', 'Silahkan check shopping cart anda :)');
+        return back();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Order  $order
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Order $order)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Order  $order
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Order $order)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Order  $order
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Order $order)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Order  $order
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Order $order)
+    {
+        //
+    }
+}
